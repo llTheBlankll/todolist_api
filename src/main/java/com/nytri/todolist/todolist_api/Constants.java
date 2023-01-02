2aa5349dfd13e531c2a0738ffe1d1ce4da23d38d
Nytri <llTheBlankll@gmail.com>
2023-01-02 09:49:47 +0800

    Added Constants for Person MAX LENGTH

--- src/main/java/com/nytri/todolist/todolist_api/Constants.java
+++ src/main/java/com/nytri/todolist/todolist_api/Constants.java
@@ -1,4 +1,8 @@
 package com.nytri.todolist.todolist_api;
 
-public class Constants {
+public  class Constants {
+
+    public static final int PERSON_FIRSTNAME_MAX_LENGTH = 64;
+    public static final int PERSON_LASTNAME_MAX_LENGTH = 64;
+
 }
